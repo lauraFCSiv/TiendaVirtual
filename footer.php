e5eaef3616efb526bda422ce2f9d171882ee9eb9
Laura <laurafraguio@gmail.com>
2023-11-13 12:24:34 +0100

    Cambio en el footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 
 <footer class="footer my-4 text-center text-lg-start fixed-bottom">
 <div class="container" style="background-color: #D7D6D8; text-align: center;">
-<span class="text-light">Síguenos en redes sociales:
+<span class="text-light">Síguenos en las redes sociales:
 <i class="bi bi-instagram"></i> <i class="bi bi-twitter"></i>
 <i class="bi bi-facebook"></i>
 </span>
